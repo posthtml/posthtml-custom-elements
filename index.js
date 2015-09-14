6ec7d2e0415931f1718bc5a24fee0c3b6fcfd3d5
Ivan Voischev <voischev.ivan@ya.ru>
2015-09-15 00:38:11 +0300

    release 0.1.0

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 module.exports = function(options) {
+    var defaultTag = options.defaultTag || 'div',
+        skipTags = options.skipTags || [],
+        html5tags = [
+        'a','abbr','address','area','article','aside','audio','b','base','bdi','bdo','blockquote',
+        'body','br','button','canvas','caption','cite','code','col','colgroup','datalist','dd','del','details','dfn',
+        'dialog','div','dl','dt','em','embed','fieldset','figcaption','figure','footer','form',
+        'h1','h2','h3','h4','h5','h6','head','header','hr','html','i','iframe','img','input','ins','kbd','keygen',
+        'label','legend','li','link','main','map','mark','menu','menuitem','meta','meter','nav','noscript','object',
+        'ol','optgroup','option','output','p','param','pre','progress','q','rp','rt','ruby','s','samp',
+        'script','section','select','small','source','span','strong','style','sub','summary','sup',
+        'table','tbody','td','textarea','tfoot','th','thead','time','title','tr','track','u','ul','var','video','wbr'
+    ];
 
     return function(tree) {
         tree.walk(function(node) {
             if(node.tag) {
-                var cl = node.tag;
-                node.tag = 'div';
-                node.attrs ?
-                    node.attrs.class = node.attrs.class + ' ' + cl :
-                    node.attrs = { class: cl };
+                var tag = node.tag;
+
+                if (skipTags.indexOf(tag) !== -1 || html5tags.indexOf(tag) === -1) {
+
+                    node.tag = defaultTag;
+
+                    if (!node.attrs) {
+                        node.attrs = { class: tag };
+                        return node;
+                    }
+
+                    var classes = node.attrs.class.split(' ');
+                    if(classes.indexOf(tag) === -1) {
+                        node.attrs.class = [tag].concat(classes).join(' ');
+                    }
+                }
             }
             return node;
         });
--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var posthtml = require('posthtml');
 var custom = require('../index.js');
 var expect = require('chai').expect;
 
-function test(html, referense, done) {
-    expect(posthtml([custom({})])
+function test(html, referense, options, done) {
+    expect(posthtml([custom(options)])
         .process(html)
         .then(function(result) {
             expect(referense).to.eql(result.html);
@@ -15,10 +15,38 @@ function test(html, referense, done) {
 }
 
 describe('Custom Elements test', function() {
-    it('test', function(done) {
-        var html = '<custom></custom>';
-        var referense = '<div class="custom"></div>';
-        test(html, referense, done);
+    describe('DOM', function() {
+        it('Simple test', function(done) {
+            var html = '<custom>Test</custom>';
+            var referense = '<div class="custom">Test</div>';
+            test(html, referense, {}, done);
+        });
+
+        it('Class', function(done) {
+            var html = '<custom class="test">Test</custom>';
+            var referense = '<div class="custom test">Test</div>';
+            test(html, referense, {}, done);
+        });
+
+        it('Class inside', function(done) {
+            var html = '<custom class="custom">Test</custom>';
+            var referense = '<div class="custom">Test</div>';
+            test(html, referense, {}, done);
+        });
+    });
+
+    describe('Options', function() {
+        it('defaultTag', function(done) {
+            var html = '<custom class="custom">Test</custom>';
+            var referense = '<span class="custom">Test</span>';
+            test(html, referense, { defaultTag: 'span'}, done);
+        });
+
+        it('skipTags', function(done) {
+            var html = '<header class="custom">Test</header>';
+            var referense = '<div class="header custom">Test</div>';
+            test(html, referense, { skipTags: ['header']}, done);
+        });
     });
 });
 
