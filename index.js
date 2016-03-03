a629317cb920c0757f64a06bcb30b23e6dbbab08
Ivan Demidov <demidov@220-volt.ru>
2016-03-03 13:44:16 +0300

    Fixed #1, convert tag to lower case if tag from html5

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ module.exports = function posthtmlCustomElements(options) {
     return function(tree) {
         tree.walk(function(node) {
             if(node.tag) {
-                var tag = node.tag.toLowerCase();
-                console.log(node.tag);
-                if (skipTags.indexOf(tag) !== -1 || html5tags.indexOf(node.tag) === -1) {
+                var tag = node.tag;
+
+                if (skipTags.indexOf(tag) !== -1 || html5tags.indexOf(tag.toLowerCase()) === -1) {
 
                     node.tag = defaultTag;
 
@@ -28,9 +28,11 @@ module.exports = function posthtmlCustomElements(options) {
                     }
 
                     var classes = node.attrs.class.split(' ');
-                    if(classes.indexOf(node.tag) === -1) {
-                        node.attrs.class = [node.tag].concat(classes).join(' ');
+                    if(classes.indexOf(tag) === -1) {
+                        node.attrs.class = [tag].concat(classes).join(' ');
                     }
+                } else {
+                    node.tag = node.tag.toLowerCase();
                 }
             }
             return node;
